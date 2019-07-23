933ec2769d14d920facb308a3478f76bf64ac8d8
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-07-23 09:17:19 +0300

    added admin profile page

--- client/src/components/Navbars/AdminNavbar.jsx
+++ client/src/components/Navbars/AdminNavbar.jsx
@@ -74,10 +74,12 @@ class AdminNavbar extends React.Component {
                   <DropdownItem className="noti-title" header tag="div">
                     <h6 className="text-overflow m-0">Welcome!</h6>
                   </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <Link to="admin/admin-profile">
+                  <DropdownItem to="/admin/admin-profile" tag={Link}>
                     <i className="ni ni-single-02" />
                     <span>profile</span>
                   </DropdownItem>
+                  </Link>
                   <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-archive-2" />
                     <span>Logs</span>
--- client/src/routes.js
+++ client/src/routes.js
@@ -20,6 +20,7 @@ import Profile from "views/examples/Profile.jsx";
 import Login from "views/examples/Login.jsx";
 import Logs from "views/examples/Tables.jsx";
 import NewSacco from "components/NewSacco.jsx";
+import AdminProfile from "views/examples/AdminProfile.jsx";
 
 var routes = [
   {
@@ -56,5 +57,11 @@ var routes = [
     component: NewSacco,
     layout: "/admin"
   },
+  {
+    path: "/admin-profile",
+    component: AdminProfile,
+    layout: "/admin"
+  },
+
 ];
 export default routes;
