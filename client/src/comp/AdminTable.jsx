f7cb5711cd757eed89e334c0fca2fafd07122a30
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-07-27 13:11:58 +0300

    form validation

---
+++ client/src/comp/AdminTable.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import { MDBCol, MDBIcon } from 'mdbreact';
+import PropTypes from 'prop-types';
+import {
+  Badge,
+  Button,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Progress,
+  Table,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  Container,
+  Row,
+  UncontrolledTooltip,
+} from 'reactstrap';
+// core components
+import Header from 'components/Headers/Header.jsx';
+import { Link } from 'react-router-dom';
+import AdminRow from 'components/AdminRow.jsx';
+
+export default class TableWhite extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+    };
+  }
+
+  render() {
+    const { data, sortQuery, handleSortChange } = this.props;
+    console.log(data);
+    const Row = data.map((value, index) => {
+      return <AdminRow key={value._id} sacco={value} />;
+    });
+    return (
+      <div>
+        <Link to="/admin/new-sacco">
+          <Button style={{ margin: '40px', float: 'right' }} color="success">
+            New Sacco
+          </Button>
+        </Link>
+        <br />
+        <UncontrolledDropdown style={{ marginTop: '20px' }} group>
+          <DropdownToggle caret color="info" data-toggle="dropdown">
+            Sort By
+          </DropdownToggle>
+          <DropdownMenu value={sortQuery} onClick={handleSortChange}>
+            <DropdownItem value="name">Sacco Name</DropdownItem>
+            <DropdownItem value="created">Date</DropdownItem>
+            <DropdownItem value="address">Location</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+        {/* search button */}
+        <MDBCol style={{ float: 'right' }} md="4">
+          <form className="form-inline mt-4 mb-4">
+            <MDBIcon icon="search" />
+            <input
+              ref={input => (this.search = input)}
+              onChange={this.handleSearchInputChange}
+              className="form-control form-control-sm ml-3 w-75"
+              type="text"
+              placeholder="Search for..."
+              aria-label="Search"
+            />
+          </form>
+        </MDBCol>
+
+        <Table className="align-items-center table-flush" responsive>
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">Sacco Name</th>
+              <th scope="col">Registered Date</th>
+              <th scope="col">Status</th>
+              <th scope="col">Contacts</th>
+              <th scope="col">Location</th>
+              <th scope="col" />
+            </tr>
+          </thead>
+
+          <tbody>{Row}</tbody>
+        </Table>
+        <CardFooter className="py-4">
+          <nav aria-label="...">
+            <Pagination
+              className="pagination justify-content-end mb-0"
+              listClassName="justify-content-end mb-0"
+            >
+              <PaginationItem className="disabled">
+                <PaginationLink
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                  tabIndex="-1"
+                >
+                  <i className="fas fa-angle-left" />
+                  <span className="sr-only">Previous</span>
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem className="active">
+                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                  1
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                  2 <span className="sr-only">(current)</span>
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                  3
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                  <i className="fas fa-angle-right" />
+                  <span className="sr-only">Next</span>
+                </PaginationLink>
+              </PaginationItem>
+            </Pagination>
+          </nav>
+        </CardFooter>
+      </div>
+    );
+  }
+}
+
+TableWhite.propTypes = {
+  data: PropTypes.array.isRequired,
+};
---
+++ client/src/comp/Headers/Header.jsx
@@ -0,0 +1,123 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="header bg-gradient-success pb-8 pt-5 pt-md-8">
+          <Container fluid>
+            <div className="header-body">
+              {/* Card stats */}
+              <Row>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardTitle
+                            tag="h5"
+                            className="text-uppercase text-muted mb-0"
+                          >
+                            Registerd Saccos
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            783
+                          </span>
+                        </div>
+
+                      </Row>
+                      
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardTitle
+                            tag="h5"
+                            className="text-uppercase text-muted mb-0"
+                          >
+                            Registered Riders
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            5,444
+                          </span>
+                        </div>
+                       
+                      </Row>
+                   
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardTitle
+                            tag="h5"
+                            className="text-uppercase text-muted mb-0"
+                          >
+                            Active Saccos
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">761</span>
+                        </div>
+                        
+                      </Row>
+                
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardTitle
+                            tag="h5"
+                            className="text-uppercase text-muted mb-0"
+                          >
+                            Deactivated Saccos
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            22
+                          </span>
+                        </div>
+                       
+                      </Row>
+                    
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Header;
